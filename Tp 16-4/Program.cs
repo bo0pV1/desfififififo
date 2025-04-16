7e9a8435b69b56818c9de6f7ebbff27642e7f567
bo0pV1 <thiago11123xd@gmail.com>
2025-04-16 20:46:36 -0300

    Add files via upload

---
+++ Tp 16-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string palabra;
+        string palabrasinespacios = string.Empty;
+        string palindromo = string.Empty;
+        Console.WriteLine("Ingrese una palabra");
+        palabra = Console.ReadLine();
+        palabra = palabra.ToLower();
+        for (int i = 0; i <= palabra.Length - 1; i++)
+        {
+            if (palabra[i] != ' ')
+            {
+                palabrasinespacios += palabra[i];
+            }
+        }
+        for (int i = palabrasinespacios.Length - 1; i >= 0; i--)
+        {
+            palindromo += palabrasinespacios[i];
+        }
+        if (palabrasinespacios == palindromo)
+        {
+            Console.WriteLine("Es un palindromo");
+        }
+        else 
+        {
+            Console.WriteLine("No es un palindromo");
+        }
+        Console.ReadKey();
+    }
+}
